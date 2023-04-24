363a481aa9981ffb2b941c068806caa6a81891dc
Ben Werner <ben.werner01@gmail.com>
2023-04-25 00:05:29 +0100

    ft: add MUI

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,17 @@
 import * as ReactDOM from "react-dom";
+import { Typography, ThemeProvider } from "@mui/material";
+import { theme } from "./mui/theme";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography>Track Selector</Typography>
+    </ThemeProvider>
+  );
+};
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(<App />, document.body);
 }
 
 render();
